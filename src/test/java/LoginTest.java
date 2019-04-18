8014b5a848f626b1beeb8898cecef72212528a0c
Emil Abkerimov <e.abkerimov@gmail.com>
2019-04-18 23:18:50 +0300

    Hometask (Lesson3)

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -3,9 +3,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
-import static java.lang.Thread.sleep;
 
 public class LoginTest {
 
@@ -14,48 +15,55 @@ public class LoginTest {
     public static String projectUrl = "https://www.linkedin.com";
     public static String userEmail = "engineertest70@gmail.com";
     public static String userPassword = "Test111)";
+    public static String userWrongPassword = "Test222)";
+    public static String welcomeMSG = "Emilio Carmello";
+    //public static String errorMSG = "Hmm, that's not the right password. Please try again or request a new one.s";
+    public static String errorMSG = "Это неверный пароль. Повторите попытку или измените пароль.";
+    
+
+/*    @BeforeTest
+    public void openProjectUrl(){}
+*/
+
 
     @Test
-    public void successfulLoginTest() {
-        System.setProperty("webdriver.chrome.driver", "C:/Users/chromedriver_win32/chromedriver.exe");
-        //System.setProperty("webdriver.chrome.driver", "/Users/emil/IdeaProjects/chromedriver");
-
-        driver = new ChromeDriver();
+    public void successfulLoginTest(){
+        //System.setProperty("webdriver.chrome.driver", "C:/Users/chromedriver_win32/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "/Users/emil/IdeaProjects/chromedriver");
+        WebDriver driver = new ChromeDriver();
         driver.get(projectUrl);
 
-        WebElement userEmailLogin = driver.findElement(By.xpath("//input[@id='login-email']"));
-        WebElement userPasswordLinkedin = driver.findElement(By.xpath("//input[@id='login-password']"));
+        WebElement loginToLinkedin = driver.findElement(By.xpath("//input[@id='login-email']"));
+        WebElement passwordToLinkedin = driver.findElement(By.xpath("//input[@id='login-password']"));
         WebElement submitToLinkedin = driver.findElement(By.xpath("//input[@id='login-submit']"));
 
         // successfulPageOpenTest
-        //Assert.assertEquals(driver.getTitle(), "LinkedIn: Войти или зарегистрироваться ");
-        Assert.assertEquals(driver.getTitle(), "LinkedIn: Log In or Sign Up ");
+        Assert.assertEquals(driver.getTitle(), "LinkedIn: Войти или зарегистрироваться ");
 
-        //successfulLoginTest
-        userEmailLogin.sendKeys(userEmail);
-        userPasswordLinkedin.sendKeys(userPassword);
+        //failedLoginTest
+        loginToLinkedin.sendKeys(userEmail);
+        passwordToLinkedin.sendKeys(userWrongPassword);
         submitToLinkedin.click();
 
-        try {
-            sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        WebElement errorMessage = driver.findElement(By.xpath("//div[@id='error-for-password']"));
+        Assert.assertEquals(errorMessage.getText(), errorMSG);
 
-        WebElement profileMenuItem = driver.findElement(By.xpath("//li[@id='profile-nav-item']"));
-        Assert.assertTrue(profileMenuItem.isDisplayed(),
-                "Home page is displayed");
+        //successfulLoginTest
+        WebElement passwordError = driver.findElement(By.xpath("//input[@id='password']"));
+        WebElement reSubmit = driver.findElement(By.xpath("//div/button"));
 
+        passwordError.sendKeys(userPassword);
+        reSubmit.click();
 
-        profileMenuItem.click();
-        WebElement userProfileName = driver.findElement(By.xpath("//ul[@id='nav-settings__dropdown-options']//h3"));
-        Assert.assertEquals(userProfileName.getText(), "Emilio Carmello", "Wrong profile user name displayed");
+        WebElement welcomeMessage = driver.findElement(By.xpath("//a[@data-control-name='identity_welcome_message']/span"));
+        Assert.assertEquals(welcomeMessage.getText(), welcomeMSG);
 
         driver.quit();
     }
 
-    @Test
-    public void negativeLoginTest(){
-        
-    }
+/*  @AfterTest
+        public void endTest(){
+            driver.quit();
+        }
+*/
 }
